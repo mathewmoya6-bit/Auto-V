4f7b9b1af6587487ed62cef814345ef35d450d30
mathewmoya6-bit <mathewmoya6@gmail.com>
2026-06-13 13:37:32 +0300

    Create supabase.js

---
+++ public/supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+'https://tsvejnzxrxrrecgquxbq.supabase.co';
+
+const SUPABASE_KEY =
+'YOUR_ANON_KEY';
+
+const supabaseClient =
+supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
