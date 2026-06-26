988c00288818c1851ae0201bcc42e8fc66f94edb
mathewmoya6-bit <mathewmoya6@gmail.com>
2026-06-26 22:51:07 +0300

    Update init.py

--- backend/utils/init.py
+++ backend/utils/init.py
@@ -1,63 +1,35 @@
-# utils/__init__.py
 """
-Utility functions for AUTO-V backend
+Utils Package
 """
 
-from .decorators import (
+from app.utils.decorators import (
     rate_limit,
     require_auth,
+    require_role,
     log_request,
     handle_errors,
-    retry_on_failure
+    retry_on_failure,
+    measure_performance,
+    cache_response,
+    public_endpoint,
+    protected_endpoint,
+    admin_endpoint,
+    get_rate_limit_status,
+    RateLimiter,
 )
-
-from .validators import (
-    validate_email,
-    validate_phone,
-    validate_vin,
-    validate_vin_format,
-    validate_odometer,
-    validate_amount,
-    validate_date
-)
-
-from .helpers import (
-    generate_id,
-    generate_reference,
-    format_currency,
-    calculate_age,
-    get_current_time,
-    sanitize_string
-)
-
-from .logger import setup_logging, get_logger
 
 __all__ = [
-    # Decorators
-    'rate_limit',
-    'require_auth',
-    'log_request',
-    'handle_errors',
-    'retry_on_failure',
-    
-    # Validators
-    'validate_email',
-    'validate_phone',
-    'validate_vin',
-    'validate_vin_format',
-    'validate_odometer',
-    'validate_amount',
-    'validate_date',
-    
-    # Helpers
-    'generate_id',
-    'generate_reference',
-    'format_currency',
-    'calculate_age',
-    'get_current_time',
-    'sanitize_string',
-    
-    # Logger
-    'setup_logging',
-    'get_logger'
+    "rate_limit",
+    "require_auth",
+    "require_role",
+    "log_request",
+    "handle_errors",
+    "retry_on_failure",
+    "measure_performance",
+    "cache_response",
+    "public_endpoint",
+    "protected_endpoint",
+    "admin_endpoint",
+    "get_rate_limit_status",
+    "RateLimiter",
 ]
