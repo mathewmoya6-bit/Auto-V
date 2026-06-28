ad7c9d7a2b87a96c8e473f22c0d618a5619fd0a0
mathewmoya6-bit <mathewmoya6@gmail.com>
2026-06-28 17:07:11 +0300

    Create __init__.py

---
+++ backend/ app/services/__init__.py
@@ -0,0 +1,11 @@
+# app/services/__init__.py
+"""
+AUTO-V Services Package
+
+Contains business logic services such as:
+- EmailService
+- VehicleService
+- ValuationService
+- PaymentService
+- AIService
+"""
