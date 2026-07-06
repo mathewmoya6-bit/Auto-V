57feb5a32b2bf7984327a68b776aee319bbdca93
mathewmoya6-bit <mathewmoya6@gmail.com>
2026-07-06 18:48:42 +0300

    Create __init__.py

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,90 @@
+"""
+Schemas Package
+Export all Pydantic models
+"""
+
+# Import all models for easy access
+from .valuation import (
+    PropertyType,
+    PropertyCondition,
+    ValuationMethod,
+    PropertyAddress,
+    PropertyFeatures,
+    ValuationRequest,
+    InstantValueRequest,
+    ComparableProperty,
+    MarketTrends,
+    ValuationForecast,
+    ValuationResponse,
+    InstantValueResponse
+)
+
+from .inspection import (
+    InspectionType,
+    InspectionStatus,
+    InspectionSeverity,
+    InspectionItem,
+    InspectionPhoto,
+    InspectionRequest,
+    InspectionReport,
+    InspectionUpdateRequest,
+    InspectionResponse,
+    InspectionChecklist
+)
+
+from .instant_value import (
+    InstantValueBaseRequest,
+    InstantValueLocationFactors,
+    InstantValueRecentSale,
+    InstantValueMarketData,
+    InstantValueDetails,
+    InstantValueComparison,
+    InstantValueResponse as InstantValueDetailedResponse,
+    InstantValueBatchRequest,
+    InstantValueBatchResponse,
+    InstantValueHistory,
+    InstantValueAccuracyMetrics,
+    DataSource
+)
+
+__all__ = [
+    # Valuation
+    'PropertyType',
+    'PropertyCondition',
+    'ValuationMethod',
+    'PropertyAddress',
+    'PropertyFeatures',
+    'ValuationRequest',
+    'InstantValueRequest',
+    'ComparableProperty',
+    'MarketTrends',
+    'ValuationForecast',
+    'ValuationResponse',
+    'InstantValueResponse',
+    
+    # Inspection
+    'InspectionType',
+    'InspectionStatus',
+    'InspectionSeverity',
+    'InspectionItem',
+    'InspectionPhoto',
+    'InspectionRequest',
+    'InspectionReport',
+    'InspectionUpdateRequest',
+    'InspectionResponse',
+    'InspectionChecklist',
+    
+    # Instant Value
+    'InstantValueBaseRequest',
+    'InstantValueLocationFactors',
+    'InstantValueRecentSale',
+    'InstantValueMarketData',
+    'InstantValueDetails',
+    'InstantValueComparison',
+    'InstantValueDetailedResponse',
+    'InstantValueBatchRequest',
+    'InstantValueBatchResponse',
+    'InstantValueHistory',
+    'InstantValueAccuracyMetrics',
+    'DataSource'
+]
