bf23233357540b111b24675dbd981c7f8d46ad24
mathewmoya6-bit <mathewmoya6@gmail.com>
2026-07-11 02:30:45 +0300

    Update __init__.py

--- backend/app/api/v1/endpoints/__init__.py
+++ backend/app/api/v1/endpoints/__init__.py
@@ -4,24 +4,9 @@ from .categories import router as categories_router
 from .vehicles import router as vehicles_router
 from .mileage import router as mileage_router
 from .instant_value import router as instant_value_router
-from .vehicle_assessments import router as vehicle_assessments_router
+from .vehicle_assessment import router as vehicle_assessment_router
 from .valuation import router as valuation_router
 from .payments import router as payments_router
 from .inspections import router as inspections_router
 from .reports import router as reports_router
 from .settings import router as settings_router
-
-__all__ = [
-    "auth_router",
-    "users_router",
-    "categories_router",
-    "vehicles_router",
-    "mileage_router",
-    "instant_value_router",
-    "vehicle_assessments_router",
-    "valuation_router",
-    "payments_router",
-    "inspections_router",
-    "reports_router",
-    "settings_router",
-]
