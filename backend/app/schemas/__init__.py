84b250de991e549acefefd32d15ef922fcdbe7a7
mathewmoya6-bit <mathewmoya6@gmail.com>
2026-07-11 01:23:20 +0300

    Update __init__.py

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,6 +1,8 @@
 from .auth import *
-from .users import UserProfile
+from .users import UserProfile, UserResponse, UserUpdate
 
 __all__ = [
     "UserProfile",
+    "UserResponse",
+    "UserUpdate",
 ]
