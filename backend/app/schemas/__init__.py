faf502df30b73ef53114a244617747e24693a37b
mathewmoya6-bit <mathewmoya6@gmail.com>
2026-07-11 00:27:02 +0300

    Update __init__.py

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,55 +1,6 @@
-from .auth import (
-    UserLogin,
-    UserRegister,
-    RefreshRequest,
-    TokenResponse,
-    UserResponse,
-)
-
+from .auth import *
 from .user import UserProfile
 
-from .valuation import (
-    ValuationRequest,
-    ValuationCreate,
-    ValuationUpdate,
-    ValuationResponse,
-    ValuationHistory,
-    ValuationStats,
-    InstantValueRequest,
-    InstantValueResponse,
-)
-
-from .vehicle import (
-    VehicleCreate,
-    VehicleUpdate,
-    VehicleResponse,
-    VehicleDetailResponse,
-)
-
 __all__ = [
-    # Authentication
-    "UserLogin",
-    "UserRegister",
-    "RefreshRequest",
-    "TokenResponse",
-    "UserResponse",
-
-    # User
     "UserProfile",
-
-    # Valuation
-    "ValuationRequest",
-    "ValuationCreate",
-    "ValuationUpdate",
-    "ValuationResponse",
-    "ValuationHistory",
-    "ValuationStats",
-    "InstantValueRequest",
-    "InstantValueResponse",
-
-    # Vehicle
-    "VehicleCreate",
-    "VehicleUpdate",
-    "VehicleResponse",
-    "VehicleDetailResponse",
 ]
